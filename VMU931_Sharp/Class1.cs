1a6d0a3e2441cfe8ef1ea71968eaa0991acf0e18
chris_g <chr33z@gmail.com>
2018-12-05 10:08:43 +0100

    Add project files.

---
+++ VMU931_Sharp/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMU931_Sharp
+{
+    public class Class1
+    {
+    }
+}
